85b8a9126268c382313e431a9486d054953bb2a3
Leology0 <leology42@gmail.com>
2022-09-14 09:34:47 -0600

    changes

---
+++ script.js
@@ -0,0 +1,9 @@
+// Nickname Generator
+document.getElementById('randNick').addEventListener("click", randomNick);
+document.getElementById('allNick').addEventListener("click", allNick);
+
+let nicknames = document.getElementById('nicknames');
+
+function randomNick() {
+
+}
