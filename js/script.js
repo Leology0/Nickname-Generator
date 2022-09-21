0add87608439fa0adde7beabbc863e04ef9043da
Leology0 <leology42@gmail.com>
2022-09-21 09:11:31 -0600

    change

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ document.getElementById('randNick').addEventListener("click", randomNick);
 document.getElementById('allNick').addEventListener("click", allNick);
 
 // Global Variables
-let firstInput = document.getElementById("firstName")
-let lastInput = document.getElementById("lastName")
-let containerEl = document.getElementById("container")
+let firstInput = document.getElementById("firstName");
+let lastInput = document.getElementById("lastName");
+let containerEl = document.getElementById("container");
 
 // Array
 let myNicks = ["the Coolest", "the Great", "the Gamer", "the Student"];
 
 // Grab a random nickname from array
 function randomNick() {
-    containerEl.innerHTML =`<div>${firstInput} ${myNicks[randomInt(0, 4)]} ${lastInput}</div>`
+    
 }
 
 // Grab all nicknames and format them correctly
